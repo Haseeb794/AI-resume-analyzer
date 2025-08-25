9d8c72bb84b9adc95751b102e9198334cbb9b334
Haseeb Tahir <haseeb.tahir974@gmail.com>
2025-08-24 23:02:25 -0400

    created and added styling to home page, navbar, resume cards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Navbar } from "~/components/navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>
+            Track your applications, get feedback, and land your dream job.
+          </h1>
+          <h2>
+            Review your submissions, get AI-powered feedback, and improve your
+            chances of getting hired.
+          </h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
